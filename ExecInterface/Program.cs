db14b2eae5517bfef65fc9532042f24342b347c4
emiliobresolin <emiliotdm@hotmail.com>
2022-05-06 12:25:01 -0300

    main ready for first test

--- ExecInterface/Program.cs
+++ ExecInterface/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ExecInterface.Entities;
+using ExecInterface.Services;
+using System;
+using System.Globalization;
 
 namespace ExecInterface
 {
@@ -6,7 +9,23 @@ namespace ExecInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int numberMonths = int.Parse(Console.ReadLine());
+            Contract newContract = new Contract(contractNumber, contractDate, contractValue);
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(newContract, numberMonths);
+            Console.WriteLine("Installments:");
+            foreach(Installment installment in newContract.Installment)
+            {
+                Console.WriteLine(installment);
+            }
         }
     }
 }
